4879d6416d3ac37f4330854afc3fa8dae39e57d0
Yarolav <yaroslav@talentpath.co>
2024-12-17 17:10:45 +0200

    Assets & Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A better version of Stack Overflow",
+  description: `A commmunity-driven platform for asking and answering programming questions.
+    Get help, share knowledge, and collaborate with developers from around the world. Eplore topics in web 
+    development, mobile app development, algorithms, data structures, and more.`,
+  icons: {
+    // icon: "/public/images/site-logo.svg",
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
